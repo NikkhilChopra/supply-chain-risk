d59520fd73e7ca13acfa1c0d1ba60d5f7568596e
NikkhilChopra <nikkhil.chopra1@gmail.com>
2025-09-24 20:16:22 +0530

    initial commit

---
+++ supply-chain-risk/src/main/java/com/nikkhil/supply_chain_risk/SupplyChainRiskApplication.java
@@ -0,0 +1,13 @@
+package com.nikkhil.supply_chain_risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainRiskApplication.class, args);
+	}
+
+}
---
+++ supply-chain-risk/src/test/java/com/nikkhil/supply_chain_risk/SupplyChainRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikkhil.supply_chain_risk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
